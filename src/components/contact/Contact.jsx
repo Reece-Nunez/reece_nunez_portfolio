e93276ecbeadc75e994d843c1e0f3dd90e37a967
Reece Nunez <reecenunez@hotmail.com>
2024-01-09 13:26:50 -0600

    changed contact header

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -58,9 +58,9 @@ const Contact = () => {
             <div
             data-aos="zoom-in"
             >
-              <h1 className="text-5xl font-bold sm:text-3xl">You Need</h1>
+              <h1 className="text-5xl font-bold sm:text-3xl">Do You</h1>
               <h3 className="text-xl sm:text-lg">
-                Beautiful design for your website leave a request
+                Want to Collaborate on a New Software Solution? Leave a Request
               </h3>
             </div>
           </div>
