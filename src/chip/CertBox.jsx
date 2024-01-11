4ed4d256a89d93d0aa0f636595488b4655c24379
Reece Nunez <116838102+Reece-Nunez@users.noreply.github.com>
2024-01-11 02:28:29 +0000

    certs now have darkmode styles

--- src/chip/CertBox.jsx
+++ src/chip/CertBox.jsx
@@ -4,7 +4,7 @@ const CertBox = ({ image, certName }) => {
     return (
         <div className="cert-box flex flex-col items-center p-4 border border-gray-300 rounded-lg shadow-sm">
         <img src={image} alt={certName} className="h-16 w-16 mb-2" /> {/* Adjust size as needed */}
-        <div className="name text-lg font-medium">{certName}</div>
+        <div className="name text-lg font-medium dark:text-white">{certName}</div>
         </div>
     );
 };
--- src/components/certs/Certs.jsx
+++ src/components/certs/Certs.jsx
@@ -12,12 +12,12 @@ const Certs = ({ darkMode }) => {
             <div className="container m-auto mt-16">
                 {/* heading */}
                 <div data-aos="fade-up" className="relative mb-5">
-                    <h3 className="text-3xl font-black text-gray-400 sm:text-2xl">
+                    <h3 className="text-3xl font-black text-gray-400 sm:text-2xl dark:text-white">
                         My Certifications
                     </h3>
                     <span className="h-[1.1px] right-0 absolute w-[90%] bg-gray-300 block"></span>
                     <div data-aos="fade-up" className="left flex-1 w-full">
-                        <p className=" text-gray-700 font-medium w-[100%]">
+                        <p className=" text-gray-700 font-medium w-[100%] dark:text-white">
                             Here are some of my certifications.
                         </p>
                     </div>
